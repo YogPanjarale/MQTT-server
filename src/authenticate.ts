214ed4fb0099ff402cca6bc87187f06bc7e92495
Yog <yog.panjarale@gmail.com>
2021-12-04 23:11:32 +0530

    Update authenticate.ts

--- src/authenticate.ts
+++ src/authenticate.ts
@@ -1,6 +1,6 @@
 import { Client } from "aedes"
 
-export default  (_client: Client,username: string, _password: Buffer  = Buffer.from("abc"), callback:(error: Error|null, successful:boolean) => void)=>{
+export default  (_client: Client,username: string="no", _password: Buffer  = Buffer.from("abc"), callback:(error: Error|null, successful:boolean) => void)=>{
     const password=_password.toString("utf-8");
     console.log("authenticating "+username+" pass: "+password)
     const lengthEqualTo= (input:string,n:number)=>{return input.length==n;}
